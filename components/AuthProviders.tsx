a883fc5f52d39f3b916da9b3f3e1e8f7cfc3f6f3
bdeamorrortu <belen@bewise.com.es>
2023-09-09 23:17:08 -0300

    feat: auth provider integrated

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,44 @@
+"use client";
 
+import { getProviders, signIn } from "next-auth/react";
+import { useState, useEffect } from "react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
-}
+  const [providers, setProviders] = useState<Providers | null>(null);
 
-export default AuthProviders
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  console.log(providers);
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <button key={i} onClick={() => signIn(provider?.id)}>
+            {provider.id}
+          </button>
+        ))}
+      </div>
+    );
+  }
+};
+
+export default AuthProviders;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,52 +1,44 @@
-import { NavLinks } from '@/constant';
-import Image from 'next/image';
-import Link from 'next/link';
-import AuthProviders from './AuthProviders';
+import { NavLinks } from "@/constant";
+import Image from "next/image";
+import Link from "next/link";
+import AuthProviders from "./AuthProviders";
 
 const Navbar = () => {
-
-    const session = {};
+  const session = null;
 
   return (
-    <nav className='flexBetween navbar'>
-        <div className='flex-1 flexStart gap-10'>
-            <Link href={"/"}>
-                <Image
-                src={"/logo.svg"}
-                width={115}
-                height={43}
-                alt='Flexibble'
-                >
-                </Image>
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href={"/"}>
+          <Image
+            src={"/logo.svg"}
+            width={115}
+            height={43}
+            alt="Flexibble"
+          ></Image>
+        </Link>
+
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
             </Link>
+          ))}
+        </ul>
 
-            <ul className='xl:flex hidden text-small gap-7'>
-                {
-                    NavLinks.map((link) => (
-                        <Link href={link.href} key={link.key}>
-                            {link.text}
-                        </Link>
-                    ))
-                }
-            </ul>
-
-            <div className='flexCenter gap-4'>
-                {
-                    session ? (
-                        <>
-                            UserPhoto
-                            <Link href={'/create-project'}>
-                                Share Work
-                            </Link>
-                        </>
-                    ) : (
-                        <AuthProviders />
-                    )
-                }
-            </div>
+        <div className="flexCenter gap-4">
+          {session ? (
+            <>
+              UserPhoto
+              <Link href={"/create-project"}>Share Work</Link>
+            </>
+          ) : (
+            <AuthProviders />
+          )}
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
